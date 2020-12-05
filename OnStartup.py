b5cd5d0399f2d85fea0d185906132cc2b389ed93
Chris Sheehan <chris_sheehan@hotmail.co.uk>
2020-12-05 16:48:44 +0000

    format change

--- OnStartup.py
+++ OnStartup.py
@@ -9,5 +9,5 @@ print("Hello (pi) World!")
 print(dt_string)
 
 file = open("test.txt", "w") 
-file.write("This file was created to make sure the boot task is working today " + dt_string) 
+file.write("This file was created to make sure the boot task is working today\n" + dt_string) 
 file.close()
