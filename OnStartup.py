365535db1c5a8939aae250d0c60dd60c6c0c8467
Chris Sheehan <chris_sheehan@hotmail.co.uk>
2020-12-05 17:58:41 +0000

    get currentDir

--- OnStartup.py
+++ OnStartup.py
@@ -8,6 +8,12 @@ print("Hello (pi) World!")
 
 print(dt_string)
 
-file = open("./test.txt", "w") 
+import os
+
+curentDir = os.path.dirname(os.path.realpath(__file__))
+
+targetOutFile = os.path.join(curentDir, "test.txt")
+
+file = open(targetOutFile, "w") 
 file.write("This file was created to make sure the boot task is working today\n" + dt_string) 
 file.close()
