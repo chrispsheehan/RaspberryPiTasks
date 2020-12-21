607e8bac8bba6023d812a35ce091b36ef3892f8f
Chris Sheehan <chris_sheehan@hotmail.co.uk>
2020-12-21 16:14:31 +0000

    removed nodejs install

--- src/Setup.py
+++ src/Setup.py
@@ -1,10 +1,5 @@
-import requests
 import subprocess
-import sys
-
-def installNode():
-    requests.get("https://deb.nodesource.com/setup_10.x")
-    subprocess.check_call([sys.executable, "apt", "install", "nodejs"])    
+import sys 
 
 def pipInstall(packages):
     for package in packages:
