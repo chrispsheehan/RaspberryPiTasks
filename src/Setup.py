abcbea6530543c5a42966a0a0ee1dd747e5dd6b4
Chris Sheehan <chris_sheehan@hotmail.co.uk>
2020-12-21 15:56:30 +0000

    remove pip code install

--- src/Setup.py
+++ src/Setup.py
@@ -10,11 +10,6 @@ def downloadFile(url, filePath):
     r = requests.get(url, allow_redirects=True)
     open(filePath, 'wb').write(r.content)
 
-def installPip():
-    pipFile = "get-pip.py"
-    downloadFile("https://bootstrap.pypa.io/get-pip.py", pipFile)
-    subprocess.check_call([sys.executable, "python", pipFile])
-
 def pipInstall(packages):
     for package in packages:
         subprocess.check_call([sys.executable, "-m", "pip", "install", package])
