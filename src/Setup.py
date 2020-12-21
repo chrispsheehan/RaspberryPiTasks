3dc071b28009d1f70a076c505ffe3bb3623849a5
Chris Sheehan <chris_sheehan@hotmail.co.uk>
2020-12-21 15:14:19 +0000

    curl fix

--- src/Setup.py
+++ src/Setup.py
@@ -2,12 +2,12 @@ import subprocess
 import sys
 
 def installNode():
-    subprocess.check_call([sys.executable, "-m", "curl", "-sL", "https://deb.nodesource.com/setup_10.x"])
-    subprocess.check_call([sys.executable, "-m", "apt", "install", "nodejs"])    
+    subprocess.check_call([sys.executable, "curl", "-sL", "https://deb.nodesource.com/setup_10.x"])
+    subprocess.check_call([sys.executable, "apt", "install", "nodejs"])    
 
 def installPip():
-    subprocess.check_call([sys.executable, "-m", "curl", "https://bootstrap.pypa.io/get-pip.py", "-o", "get-pip.py"])
-    subprocess.check_call([sys.executable, "-m", "python", "get-pip.py"])    
+    subprocess.check_call([sys.executable, "curl", "https://bootstrap.pypa.io/get-pip.py", "-o", "get-pip.py"])
+    subprocess.check_call([sys.executable, "python", "get-pip.py"])    
 
 def pipInstall(packages):
     for package in packages:
