86e448708f5453ce650ebec4a791d92f1bf21752
Chris Sheehan <chris_sheehan@hotmail.co.uk>
2020-12-21 18:33:18 +0000

    6l node version

--- Init.sh
+++ Init.sh
@@ -1,5 +1,5 @@
 git clone https://github.com/chrispsheehan/RaspberryPiTasks.git
-curl -sL https://deb.nodesource.com/setup_10.x | bash
+curl -sL https://nodejs.org/dist/v8.11.3/node-v8.11.3-linux-armv6l.tar.xz | bash
 apt install nodejs
 curl "https://bootstrap.pypa.io/get-pip.py" -o "get-pip.py"
 python get-pip.py
