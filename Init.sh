1305f88c252e3a6f9a97efd48ba337d88bfe55d6
Chris Sheehan <chris_sheehan@hotmail.co.uk>
2020-12-22 11:37:45 +0000

    clean dir before pull

--- Init.sh
+++ Init.sh
@@ -4,6 +4,7 @@ apt install nodejs
 curl "https://bootstrap.pypa.io/get-pip.py" -o "get-pip.py"
 python get-pip.py
 cd RaspberryPiTasks
+git clean -df
 git pull
 cd ..
 python ./RaspberryPiTasks/src/EntryPoint.py '-init'
