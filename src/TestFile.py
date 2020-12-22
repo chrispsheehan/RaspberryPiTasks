a25f5bcdb4036f66947a3bbe3a807f3fca7c0345
Chris Sheehan <chris_sheehan@hotmail.co.uk>
2020-12-22 15:32:33 +0000

    update every hour

--- src/TestFile.py
+++ src/TestFile.py
@@ -1,10 +1,19 @@
+import os
+
 import DateTime
 
-import OS
+
+def writeOutFile(dt_string):
+    curentDir = os.path.dirname(os.path.realpath(__file__))
+
+    targetOutFile = os.path.join(curentDir, "test.txt")
+
+    file = open(targetOutFile, "w")
+    file.write("Boot job working :) - what a time to be alive\n" + dt_string)
+    file.close()
 
 
 def writeOut():
     dateTime = DateTime.dateStamp()
 
-    OS.writeOutFile(dateTime)
-    
+    writeOutFile(dateTime)
